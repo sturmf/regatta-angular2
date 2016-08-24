42b34c9f070a36198a3091e4bb884d2acb0d35ea
Fabian Sturm <f@rtfs.org>
2016-08-24 20:20:30 +0200

    Added more event data.

--- regatta/events/models.py
+++ regatta/events/models.py
@@ -11,10 +11,10 @@ class Person(models.Model):
 
 
 class SailingClub(models.Model):
-    name = models.CharField(max_length=200, blank=True, help_text="full name of the sailing club")
-    abbreviation = models.CharField(max_length=20, blank=True, help_text="official abbreviation of the sailing club")
-    registration = models.CharField(max_length=20, blank=True, help_text="the registration number of the sailing club, if applicable")
-    was_organizer = models.BooleanField(default=False, help_text="if this sailing club has ever been an organizer")
+    name = models.CharField(max_length=200, blank=True, help_text='full name of the sailing club')
+    abbreviation = models.CharField(max_length=20, blank=True, help_text='official abbreviation of the sailing club')
+    registration = models.CharField(max_length=20, blank=True, help_text='the registration number of the sailing club, if applicable')
+    was_organizer = models.BooleanField(default=False, help_text='if this sailing club has ever been an organizer')
 
     def __str__(self):
         return self.name
@@ -29,13 +29,16 @@ class Event(models.Model):
         (CLASS, 'Class'),
     )
 
-    name = models.CharField(max_length=200, blank=True, help_text="name of the event")
-    mode = models.IntegerField(choices=MODE_CHOICES, default=YARDSTICK, help_text="type of the event either Class or Yardstick")
-    start_date = models.DateField(default=date.today, help_text="first day of the event")
-    end_date = models.DateField(default=date.today, help_text="last day of the event")
-    race_count = models.PositiveIntegerField(default=1, help_text="amount of races scheduled during the event")
-    organizer = models.ForeignKey(SailingClub, help_text="the sailing club that organizes the event")
-    race_committee = models.ForeignKey(Person, help_text="the person responsible for the execution of the event")
+    name = models.CharField(max_length=200, blank=True, help_text='name of the event')
+    mode = models.IntegerField(choices=MODE_CHOICES, default=YARDSTICK, help_text='type of the event either Class or Yardstick')
+    start_date = models.DateField(default=date.today, help_text='first day of the event')
+    end_date = models.DateField(default=date.today, help_text='last day of the event')
+    race_count = models.PositiveIntegerField(default=1, help_text='amount of races scheduled during the event')
+    organizer = models.ForeignKey(SailingClub, help_text='the sailing club that organizes the event')
+    race_committee = models.ForeignKey(Person, help_text='the person responsible for the execution of the event')
+    race_unrated_on = models.CharField(max_length=100, blank=True, help_text='describing after how many race one is unaccounted for')
+    umpire = models.ForeignKey(Person, help_text='the presiding judge')
+    assistants = models.ManyToManyField(Person, help_text='everybody else helping in the event')
 
     def __str__(self):
         return self.name
