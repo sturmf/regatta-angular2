1a3e36218a7b244cf64de7b3bb8b0ef8d70c96d0
Fabian Sturm <f@rtfs.org>
2016-08-24 22:02:22 +0200

    Nicer admin view

--- regatta/events/admin.py
+++ regatta/events/admin.py
@@ -5,8 +5,14 @@ from .models import SailingClub
 from .models import BoatType
 from .models import Entry
 
+
+class EntryAdmin(admin.ModelAdmin):
+    model = Entry
+    list_display = ('event', 'helm', 'boat_type')
+
+
 admin.site.register(Event)
 admin.site.register(Person)
 admin.site.register(SailingClub)
 admin.site.register(BoatType)
-admin.site.register(Entry)
+admin.site.register(Entry, EntryAdmin)
