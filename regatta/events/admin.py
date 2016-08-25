df3d932808a5926c57a71fbdc66674a6aff5e974
Fabian Sturm <f@rtfs.org>
2016-08-25 21:48:57 +0200

    polish admin a bit

--- regatta/events/admin.py
+++ regatta/events/admin.py
@@ -6,12 +6,17 @@ from .models import BoatType
 from .models import Entry
 
 
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+    list_display = ('name', 'mode', 'start_date', 'end_date', 'race_count', 'race_unrated_on', 'organizer', 'race_committee', 'umpire')
+
+
 class EntryAdmin(admin.ModelAdmin):
     model = Entry
     list_display = ('event', 'helm', 'crew_names', 'boat_type')
 
 
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Person)
 admin.site.register(SailingClub)
 admin.site.register(BoatType)
