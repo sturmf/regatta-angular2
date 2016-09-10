37fdfe203ba2c31df2bb24af7e8b72f55f275c93
Fabian Sturm <f@rtfs.org>
2016-09-10 08:11:21 +0200

    For now allow everyone to access the rest api.

--- regatta/regatta/settings.py
+++ regatta/regatta/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'PAGE_SIZE': 10
 }
 
