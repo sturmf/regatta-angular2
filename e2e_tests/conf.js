54c09da5774568124b44d8ba0ec2aaa74dd272b6
gesellc <christian.gesell@t-online.de>
2016-09-23 22:20:59 +0200

    temporarily remove usage of jasmine-spec-reporter

--- e2e_tests/conf.js
+++ e2e_tests/conf.js
@@ -1,13 +1,13 @@
-var SpecReporter = require('/usr/local/lib/node_modules/jasmine-spec-reporter');
+// var SpecReporter = require('/usr/local/lib/node_modules/jasmine-spec-reporter');
 
 exports.config = {
   capabilities: {
     'browserName': 'firefox'
   },
-  onPrepare: function() {
-     // add jasmine spec reporter
-     jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'none'}));
-  },
+//  onPrepare: function() {
+//     // add jasmine spec reporter
+//     jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'none'}));
+//  },
   framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['regatta-spec.js'],
