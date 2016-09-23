12afb4c012327657019a8c65309236c6bb185915
Fabian Sturm <f@rtfs.org>
2016-09-23 18:11:40 +0200

    Add some more event fields.

--- frontend/lib/event.dart
+++ frontend/lib/event.dart
@@ -5,8 +5,18 @@ class Event {
   final String url;
   String name;
   DateTime startDate;
+  DateTime endDate;
+  int race_count;
+  String race_unrated_on;
+  // organizer
+  // race_committee
+  // umpire
+  // assistants
+  // entries
+  // races
 
-  Event(this.id, this.url, this.name, this.startDate);
+
+  Event(this.id, this.url, this.name, this.startDate, this.endDate, this.race_count, this.race_unrated_on);
 
   String get startDateStr {
     var formatter = new DateFormat('yyyy-MM-dd');
@@ -17,11 +27,32 @@ class Event {
     startDate = DateTime.parse(date);
   }
 
+  String get endDateStr {
+    var formatter = new DateFormat('yyyy-MM-dd');
+    return formatter.format(endDate);
+  }
+
+  void set endDateStr(String date) {
+    endDate = DateTime.parse(date);
+  }
 
   factory Event.fromJson(Map<String, dynamic> event) =>
-    new Event(_toInt(event['id']), event['url'], event['name'], DateTime.parse(event['start_date']));
+    new Event(_toInt(event['id']),
+        event['url'], event['name'],
+        DateTime.parse(event['start_date']),
+        DateTime.parse(event['end_date']),
+        _toInt(event['race_count']),
+        event['race_unrated_on']
+    );
 
-  Map toJson() => {'id': id, 'url': url, 'name': name, 'start_date': startDateStr};
+  Map toJson() => {'id': id,
+    'url': url,
+    'name': name,
+    'start_date': startDateStr,
+    'end_date': endDateStr,
+    'race_count': race_count,
+    'race_unrated_on': race_unrated_on
+  };
 }
 
 int _toInt(id) => id is int ? id : int.parse(id);
