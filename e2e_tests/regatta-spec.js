397bd95f7cd1a144b9373e8a915bc844b02e1d96
gesellc <christian.gesell@t-online.de>
2016-09-26 18:57:41 +0200

    remove comments that really where notes

--- e2e_tests/regatta-spec.js
+++ e2e_tests/regatta-spec.js
@@ -14,14 +14,4 @@ describe('The Regatta welcome view', function() {
     expect('apple').toEqual('orange');
   });
 
-
-  // - The Regatta welcome view should display the application\'s name.
-  //
-  // - Given the user is at the Regatta welcome view,
-  //   and the list of regatta items is not empty,
-  //   when he clicks a regatta item,
-  //   then the details view of the clicked regatta item is opened.
-
-
-
 });
