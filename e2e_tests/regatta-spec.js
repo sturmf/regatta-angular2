eaecb1abe5ab162973846cf2b6c511ba77f19f16
gesellc <christian.gesell@t-online.de>
2016-10-16 17:04:19 +0200

    goes down to service

--- e2e_tests/regatta-spec.js
+++ e2e_tests/regatta-spec.js
@@ -1,9 +1,26 @@
 describe('The Regatta welcome view', function() {
+  var heading = element(by.css('h1'));
+  var newRegattaForm = element(by.css('input'));
+  var addButton = element(by.buttonText('Add'));
+  var regattaList = element.all(by.repeater('let event of events'));
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080/');
+  });
 
   it('should display the application\'s name.', function() {
-    browser.get('http://localhost:8080/');
-    var heading = element(by.css('h1'));
     expect(heading.getText()).toEqual('Regatta');
+  });
+
+  it('should start with an empty list.', function() {
+    expect(regattaList.count()).toEqual(0);
+  });
+
+  it('should add a new regatta item.', function() {
+    newRegattaForm.sendKeys('Christmas Race');
+    addButton.click();
+    expect(regattaList.count()).toEqual(1);
+    expect(regattaList).;
   });
 
   xit('should show a list of all regattas.', function() {
