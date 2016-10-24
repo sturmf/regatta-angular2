582cbf9ceb7edfdef2c4ba3af8e44e45ea6cddc0
gesellc <christian.gesell@t-online.de>
2016-10-24 21:15:42 +0200

    Works on at least one OS X machine

---
+++ tmux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SESSION=$USER
+
+tmux -2 new-session -d -s $SESSION
+
+tmux new-window -t $SESSION:2 -n 'Django-Backend'
+tmux new-window -t $SESSION:3 -n 'Angular2-Frontend'
+tmux new-window -t $SESSION:4 -n 'Selenium-TestServer'
+tmux new-window -t $SESSION:5 -n 'Protractor-End2EndTest'
+
+tmux send-keys -t $SESSION:2 'cd backend'   ENTER 'make' ENTER
+tmux send-keys -t $SESSION:3 'cd frontend'  ENTER 'make' ENTER
+tmux send-keys -t $SESSION:4 'cd e2e_tests' ENTER 'make' ENTER
+
+tmux send-keys -t $SESSION:5 'cd e2e_tests' ENTER 'make test'
+
+tmux attach
