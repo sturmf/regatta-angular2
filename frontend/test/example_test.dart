213cd228a64f5242d9c45fa1fe8b628aaf802823
Fabian Sturm <f@rtfs.org>
2016-11-13 12:32:07 +0100

    Small fix to use now built in package for testing.

--- frontend/test/example_test.dart
+++ frontend/test/example_test.dart
@@ -1,8 +1,10 @@
 // Because Angular is using dart:html, we need these tests to run on an actual
 // browser. This means that it should be run with `-p dartium` or `-p chrome`.
 @TestOn('browser')
+import 'package:angular2/angular2.dart';
+import 'package:angular2/src/testing/angular2_testing.dart';
+import 'package:angular2/testing.dart';
 import "package:test/test.dart";
-import 'package:angular2_testing/angular2_testing.dart';
 
 void main() {
   initAngularTests();
