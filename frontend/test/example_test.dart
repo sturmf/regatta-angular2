ae2a2ef6f02e222b2fcf0b2c1e8d0e1aede933f3
Fabian Sturm <f@rtfs.org>
2016-11-15 20:11:49 +0100

    broke testbed implementation

--- frontend/test/example_test.dart
+++ frontend/test/example_test.dart
@@ -1,28 +1,50 @@
 // Because Angular is using dart:html, we need these tests to run on an actual
 // browser. This means that it should be run with `-p dartium` or `-p chrome`.
 @TestOn('browser')
-//import 'package:angular2/angular2.dart';
-import 'package:angular2/src/testing/angular2_testing.dart';
-import 'package:angular2/testing.dart';
+import 'dart:async';
+
+// Replaced with code generation.
+import 'package:angular2/src/core/reflection/reflection.dart';
+import 'package:angular2/src/core/reflection/reflection_capabilities.dart';
+
+// Experimental. Will be published under package:angular2/testing.dart soon.
+import 'package:angular2/src/modules/testing/lib/testing.dart';
+
+//import 'package:angular2_api_examples.testing/api.dart';
+//import 'package:angular2_api_examples.testing/ui.dart';
+//import 'package:angular2_api_examples.testing/testing.dart';
+
+import 'package:angular2/angular2.dart';
+//import 'package:mockito/mockito_no_mirrors.dart';
+//import 'package:pageloader/html.dart';
 import 'package:test/test.dart';
 
 import 'package:frontend/event_list_component.dart';
 
+
+@Component(
+    selector: 'test-cmp',
+    template: '<textarea \n\n</textarea>')
+class TestComponent {}
+
+
 void main() {
-  initAngularTests();
+  reflector.reflectionCapabilities = new ReflectionCapabilities();
 
   test("String.split() splits the string on the delimiter", () {
     final string = "foo,bar,baz";
     expect(string.split(","), equals(["foo", "bar", "baz"]));
   });
 
-  
-  ngTest('New event list component is empty', (TestComponentBuilder testComponentBuilder) async {
-    final eventListComponent = await testComponentBuilder.createAsync(EventListComponent);
-    eventListComponent.detectChanges();
 
-    print(eventListComponent);
-    
+  test('New event list component is empty', () async {
+    print('test start');
+    var testBed = new NgTestBed<TestComponent>();
+    var fixture = await testBed.create();
+
+
+    print(fixture);
+    //expect(string.split(","), equals(["foo", "bar", "baz"]));
     //eventListComponent.debugElement.queryAll(());
     //nativeElement.
     //.text, equals('1;2;'));
