820b267a6daaa00c83ce83286f070da6a10a3e0b
Christian Gesell <christian.gesell@t-online.de>
2016-12-01 00:49:49 +0100

    refactor protractor tests

---
+++ e2e_tests/page_objects/event_list.page.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var EventListPage = function () {
+  browser.get('http://localhost:8080/');
+};
+
+ EventListPage.prototype = Object.create({}, {
+    heading: { get: function () { return element(by.css('h1')).getText(); }},
+    newEventForm: { get: function () { return element(by.css('input')); }},
+    addButton: { get: function () { return element(by.id('addButton')); }},
+    eventList: { get: function () { return element(by.id('eventList')).all(by.css('material-button')); }},
+    eventListLength: { get: function () { return this.eventList.count(); }},
+
+    clickAddButton: { value: function () { this.addButton.click(); }},
+    typeIntoNewEventForm: { value: function (input) { this.newEventForm.sendKeys(input); }}
+ });
+
+module.exports = EventListPage;
