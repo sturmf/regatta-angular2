26130d5aaf44573eabfc89558610e76e56533012
Christian Gesell <christian.gesell@t-online.de>
2016-12-02 07:39:36 +0100

    test direct connect without seleniumAdress

--- e2e_tests/conf.js
+++ e2e_tests/conf.js
@@ -7,13 +7,14 @@ exports.config = {
   onPrepare: function() {
      jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'none'}));
   },
+  directConnect: true,  // see also https://github.com/angular/protractor/blob/master/docs/server-setup.md
   framework: 'jasmine2',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['regatta-spec.js'],
   useAllAngular2AppRoots: true,
 };
