4e8233aa2cb2477acd2636bd4b46a835da62213c
Christian Gesell <christian.gesell@t-online.de>
2017-04-01 13:07:26 +0200

    coverage test file for all UTs

---
+++ frontend/test/all_unit_tests_for_coverage.dart
@@ -0,0 +1,22 @@
+import '../lib/models/boat_test.dart' as test_01;
+import '../lib/models/boat_test.dart' as test_02;
+import '../lib/models/boat_test.dart' as test_03;
+import '../lib/models/sailing_club_test.dart' as test_04;
+import '../lib/models/boat_test.dart' as test_05;
+import '../lib/models/boat_test.dart' as test_06;
+import '../lib/models/event_test.dart' as test_07;
+import '../lib/models/event_test.dart' as test_08;
+import '../lib/models/event_test.dart' as test_09;
+
+
+void main() {
+  test_01.main();
+  test_02.main();
+  test_03.main();
+  test_04.main();
+  test_05.main();
+  test_06.main();
+  test_07.main();
+  test_08.main();
+  test_09.main();
+}
