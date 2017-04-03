3f565a4bb254df42c88c246270f0f59af1d2b0de
Christian Gesell <christian.gesell@t-online.de>
2017-04-04 00:30:39 +0200

    remove unused import

--- frontend/test/all_unit_tests_for_coverage.dart
+++ frontend/test/all_unit_tests_for_coverage.dart
@@ -2,8 +2,6 @@ import '../lib/models/boat_test.dart' as test_01;
 import '../lib/models/sailing_club_test.dart' as test_02;
 import '../lib/models/event_test.dart' as test_03;
 import '../lib/models/person_test.dart' as test_04;
-// ignore: unused_import
-import '../lib/store/regatta_reducer.dart' as coverage_hack_01;
 
 void main() {
   test_01.main();
