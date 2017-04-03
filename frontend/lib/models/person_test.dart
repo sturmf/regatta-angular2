b9be22755843adc8c150da92b69e598ac8eaf946
Christian Gesell <christian.gesell@t-online.de>
2017-04-03 22:29:13 +0200

    Add new UT to coverage test file, test unused import

--- frontend/lib/models/person_test.dart
+++ frontend/lib/models/person_test.dart
@@ -2,8 +2,8 @@ import 'package:test/test.dart';
 import 'package:frontend/models/person.dart';
 
 void main() {
-
   test('instantiate person', () {
-    final person = new Person(2012,'myUrl', 'John', 'Doe', 'mySailingClub');
+    final person = new Person(2012, 'myUrl', 'John', 'Doe', 'mySailingClub');
+    expect('John', person.firstName);
   });
 }
--- frontend/test/all_unit_tests_for_coverage.dart
+++ frontend/test/all_unit_tests_for_coverage.dart
@@ -1,9 +1,13 @@
 import '../lib/models/boat_test.dart' as test_01;
 import '../lib/models/sailing_club_test.dart' as test_02;
 import '../lib/models/event_test.dart' as test_03;
+import '../lib/models/person_test.dart' as test_04;
+// ignore: unused_import
+import '../lib/store/regatta_reducer.dart' as coverageHack_01;
 
 void main() {
   test_01.main();
   test_02.main();
   test_03.main();
+  test_04.main();
 }
