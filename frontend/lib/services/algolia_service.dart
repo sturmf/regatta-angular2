da5b2622a329a8790af8d97bac2b4749771eb853
Fabian Sturm <f@rtfs.org>
2018-06-09 16:13:55 +0200

    Fix test parameters

--- frontend/lib/services/algolia_service.dart
+++ frontend/lib/services/algolia_service.dart
@@ -7,7 +7,7 @@ import 'package:http/browser_client.dart' as http;
 
 @Injectable()
 class AlgoliaService {
-  final _event_url = "https://DXJQDY655R-dsn.algolia.net/1/indexes/events/query";
+  final _eventUrl = "https://DXJQDY655R-dsn.algolia.net/1/indexes/events/query";
   final _headers = {'X-Algolia-Application-Id': 'DXJQDY655R', 'X-Algolia-API-Key': 'd25d9cff0c3595b2a39d68592ea31197'};
 
   //http.BaseClient _client;
@@ -26,7 +26,7 @@ class AlgoliaService {
     final query = Uri.encodeQueryComponent(searchTerm);
     final body = '{"params": "query=$query&hitsPerPage=$pageSize&page=$page"}';
 
-    final response = await _client.post(_event_url, headers: _headers, body: body);
+    final response = await _client.post(_eventUrl, headers: _headers, body: body);
     if (response.statusCode == 200) {
       return JSON.decode(response.body) as Map<String, dynamic>;
     }
--- frontend/lib/services/algolia_service_test.dart
+++ frontend/lib/services/algolia_service_test.dart
@@ -4,7 +4,7 @@ import 'package:frontend/services/algolia_service.dart';
 void main() {
   test('check if Algolia query response contains hits', () async {
     final service = new AlgoliaService();
-    final result = await service.search();
+    final result = await service.search('', 0, 5);
     print('Result: ${result}');
     expect(result, contains('hits'));
   });
